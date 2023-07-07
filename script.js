86d316695650dc7376e15c6c0da7b41ab1c383d2
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-07-08 03:25:16 +0700

    test1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle class active
+function myFunction() {
+    var x = document.getElementById("navbar-nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
